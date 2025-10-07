1524b5f64254cc070e50ad4aae5a035cfcaeb891
Praveen <anumulapraveen30@gmail.com>
2025-10-07 10:34:36 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-// Simple "Hello World" custom action
 const core = require('@actions/core');
 
 async function run() {
   try {
     const name = core.getInput('name');
-    console.log(`👋 Hello, ${name}! This is from custom action-repo1.`);
+    console.log(`👋 Hello, $Harsha! This is from custom action-repo1.`);
   } catch (error) {
     core.setFailed(error.message);
   }
